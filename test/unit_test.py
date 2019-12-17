182c929b753a86eb13bd48004aee26fb5b758151
Rafael <rafael.sterzinger@gmx.at>
2019-12-17 22:05:27 +0100

    fix config

--- test/unit_test.py
+++ test/unit_test.py
@@ -7,7 +7,7 @@ class TestSum(unittest.TestCase):
         self.assertEqual(sum([1, 2, 3]), 6, "Should be 6")
 
     def test_sum_tuple(self):
-        self.assertEqual(sum((1, 2, 2)), 6, "Should be 6")
+        self.assertEqual(sum((2, 2, 2)), 6, "Should be 6")
 
 if __name__ == '__main__':
     unittest.main()
